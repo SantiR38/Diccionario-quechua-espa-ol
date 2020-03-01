7d4a140e9d0ecae5824e31630a04f879462bfd7e
SantiR38 <47932560+SantiR38@users.noreply.github.com>
2020-03-01 16:25:12 -0300

    Add files via upload
    Cambios realizados:
    1. Enlazar tecla 'enter' con funcion buscarPalabra()
    2. Documentación mas detallada
    3. Condicional para cuando no se escribió ninguna palabra en el Entry(), o sea, respuesta específica para cuando el valor de búsqueda es nulo

--- SajraDicc.py
+++ SajraDicc.py
@@ -2,33 +2,39 @@ from lista_palabras import *
 from tkinter import *
 import sqlite3
 
-# ----------------------Por consola con lista de palabras en un diccionario python----------------------------
 
-'''palabra = input("Escribe la palabra en quechua que buscas: ")
+# ----------------------------------------Funciones--------------------------------------------
 
-print(palabra.upper() + ". " + dicc_qu_esp[palabra.upper()])'''
-
-
-def buscarPalabra():
+def buscarPalabra(event):
+    """
+    :param event: el parametro no tiene ninguna aplicación mas que permitir que al presionar 'enter' se ejecute
+    la funcion. Por eso en el bind del entry, el parametro event tiene solo un string de relleno.
+    :return: La funcion no devuelve ningun valor, sino mas bien, rellena el widge Text() con la definicion y traducción
+    de la palabra buscada.
+    """
     if varOption.get() == 1:
         try:
             palabraBuscada = (miBusqueda.get()).lower()
-            palabra_y_definicion = palabraBuscada + ". " + dicc_esp_qu[palabraBuscada.lower()]
-            resultado.delete(1.0, END)
-            resultado.insert(1.0, palabra_y_definicion)
-            # # En caso de que el resultado se muestre en un Entry() y no en un Text()
-            # resultado.config(text=palabra_y_definicion)
+            if palabraBuscada != "":
+                palabra_y_definicion = palabraBuscada + ". " + dicc_esp_qu[palabraBuscada.lower()]
+                resultado.delete(1.0, END)
+                resultado.insert(1.0, palabra_y_definicion)
+            else:
+                resultado.delete(1.0, END)
+                resultado.insert(1.0, "Por favor, escribe una palabra...")
         except KeyError:
             resultado.delete(1.0, END)
             resultado.insert(1.0, "No hay resultado para la palabra que buscas...")
     elif varOption.get() == 2:
         try:
             palabraBuscada = (miBusqueda.get()).lower()
-            palabra_y_definicion = palabraBuscada + ". " + dicc_qu_esp[palabraBuscada.lower()]
-            resultado.delete(1.0, END)
-            resultado.insert(1.0, palabra_y_definicion)
-            # # En caso de que el resultado se muestre en un Entry() y no en un Text()
-            # resultado.config(text=palabra_y_definicion)
+            if palabraBuscada != "":
+                palabra_y_definicion = palabraBuscada + ". " + dicc_qu_esp[palabraBuscada.lower()]
+                resultado.delete(1.0, END)
+                resultado.insert(1.0, palabra_y_definicion)
+            else:
+                resultado.delete(1.0, END)
+                resultado.insert(1.0, "Por favor, escribe una palabra...")
         except KeyError:
             resultado.delete(1.0, END)
             resultado.insert(1.0, "No hay resultado para la palabra que buscas...")
@@ -75,6 +81,8 @@ titulo = Label(frameTitulo, text="Diccionario Quechua - Español")
 titulo.grid(row=0, column=0, columnspan=2, padx=5, pady=10)
 titulo.config(font=('bold', 20), bg=color_fondo)
 
+# ---------------------------Botones para cambiar de diccionario-----------------------------
+
 varOption = IntVar()
 opcion_esp_qu = Radiobutton(frameTitulo, text="Español - Quechua", variable=varOption, value=1)
 opcion_esp_qu.grid(row=1, column=0)
@@ -89,22 +97,25 @@ frameContenido = Frame()
 frameContenido.pack()
 frameContenido.config(bg=color_fondo, width="850", height="350", bd=20)
 
+# -----------------Sección donde se coloca la palabra que se quiere buscar-------------------
+
 miBusqueda = StringVar()
 busquedaLabel = Label(frameContenido, text="Busqueda: ")
 busquedaLabel.grid(row=0, column=0, sticky="e", padx=1)
 busquedaLabel.config(bg=color_fondo)
+
 cuadroTexto1 = Entry(frameContenido, textvariable=miBusqueda)
 cuadroTexto1.grid(row=0, column=1, columnspan=2)
 cuadroTexto1.config(fg="#261201", justify="center", bg=color_cuadro_texto)
+#  Esto es para que cuando se apreta 'enter', se ejecute la misma función que con el botón 'buscar'
+cuadroTexto1.bind('<Return>', buscarPalabra)
 
 # --------------------------------------Botón----------------------------------------------
-calcularBoton = Button(frameContenido, text="Buscar", command=buscarPalabra)
+# El parámetro de relleno de la siguiente línea es explicado en la documentacion de la función buscarPalabra()
+calcularBoton = Button(frameContenido, text="Buscar", command=lambda: buscarPalabra("parametro_de_relleno"))
 calcularBoton.grid(row=1, column=0, padx=5, pady=10, columnspan=3)
 
-# resultado = Label(frameContenido)
-# resultado. grid(row=7, column=0, padx=5, pady=10, columnspan=4)
-# resultado.config(width="50", height="5")
-
+# -----------------Cuadro donde se muestra el resultado de la búsqueda---------------------
 resultado = Text(frameContenido, width=40, height=12)
 resultado.grid(row=3, column=0, sticky="e", padx=10, pady=10, columnspan=3)
 resultado.config(bg=color_cuadro_texto)
